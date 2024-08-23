a24641515295b3941213365143197f391516c03c
0xsambit <177391633+0xsambit@users.noreply.github.com>
2024-08-24 00:53:21 +0530

    Sign in page made

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,14 +1,54 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+	Image,
+	ImageBackground,
+	ScrollView,
+	StyleSheet,
+	Text,
+	View,
+} from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 const SignIn = () => {
 	return (
-		<View>
-			<Text>SignIn</Text>
-		</View>
+		<SafeAreaView>
+			<ImageBackground
+				source={require("../../assets/images/sky-beach.png")}
+				style={styles.bgImage}
+				resizeMode='cover'>
+				<View style={styles.viewContainer1}>
+					<Text style={styles.title}>Riptide</Text>
+					<Image
+						source={require("../../assets/icons/vectorWhite.png")}
+						style={{ bottom: 35 }}
+					/>
+					<Text style={styles.subtitle}>Sign In</Text>
+				</View>
+			</ImageBackground>
+			<StatusBar backgroundColor='black' />
+		</SafeAreaView>
 	);
 };
 
 export default SignIn;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	bgImage: {
+		height: "100%",
+	},
+	viewContainer1: {
+		justifyContent: "center",
+		alignItems: "center",
+		marginTop: 10,
+	},
+	title: {
+		color: "white",
+		fontSize: 90,
+		fontFamily: "Allison-Regular",
+	},
+	subtitle: {
+		color: "white",
+		fontSize: 36,
+	},
+});
