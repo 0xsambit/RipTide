4a447a559edf52ab5da53a11e0d01947c060c943
0xsambit <177391633+0xsambit@users.noreply.github.com>
2024-08-27 23:44:50 +0530

    Added Home page to Tabs layout

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Stack } from "expo-router";
 
 const TabsLayout = () => {
 	return (
-		<View>
-			<Text>TabsLayout</Text>
-		</View>
+		<Stack>
+			<Stack.Screen name='home' options={{ headerShown: false }} />
+		</Stack>
 	);
 };
 
