0c18acce6b79452076d5b275a81d0acca9461990
0xsambit <177391633+0xsambit@users.noreply.github.com>
2024-09-07 23:53:00 +0530

    Images updated

--- constants/image.js
+++ constants/image.js
@@ -1,17 +1,18 @@
 import beach1 from "../assets/images/beach1.jpeg";
-import beach2 from "../assets/images/beach2.png";
 import beach3 from "../assets/images/beach3.jpg";
 import beach4 from "../assets/images/beach4.jpg";
 import beach5 from "../assets/images/beach5.jpg";
 import beach6 from "../assets/images/beach6.jpg";
-import sky from "../assets/images/sky-beach.png";
-
+import digha from "../assets/images/digha.png";
+import goa from "../assets/images/goa.png";
+import lakshadweep from "../assets/images/lakshadweep.png";
 export default images = {
 	beach1,
-	beach2,
 	beach3,
 	beach4,
 	beach5,
 	beach6,
-	sky,
+	digha,
+	goa,
+	lakshadweep,
 };
