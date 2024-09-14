f4d4304776c4e9cf3a950bc5592c351d12f197a7
0xsambit <177391633+0xsambit@users.noreply.github.com>
2024-09-14 21:25:08 +0530

    Tabs layput navigation made

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,12 +1,88 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Stack } from "expo-router";
-
+import { Stack, Tabs } from "expo-router";
+import { icons } from "../../constants";
+const TabIcon = ({ color, focused, icon, name }) => {
+	return (
+		<View style={{ alignItems: "center", justifyContent: "center" }}>
+			<Image
+				source={icon}
+				resizeMode='contain'
+				tintColor={color}
+				style={{ width: 22, height: 22 }}
+			/>
+			<Text
+				style={{
+					color: color,
+					fontFamily: "Poppins-Regular",
+					fontSize: 12,
+				}}>
+				{name}
+			</Text>
+		</View>
+	);
+};
 const TabsLayout = () => {
 	return (
-		<Stack>
-			<Stack.Screen name='home' options={{ headerShown: false }} />
-		</Stack>
+		<>
+			<Tabs
+				screenOptions={{
+					tabBarActiveTintColor: "#1f292a",
+					tabBarInactiveTintColor: "#666666",
+					tabBarShowLabel: false,
+					tabBarStyle: {
+						backgroundColor: "#AFD6DA",
+						paddingTop: 10,
+						height: 50,
+					},
+				}}>
+				<Tabs.Screen
+					name='home'
+					options={{
+						title: "Home",
+						headerShown: false,
+						tabBarIcon: ({ color, focused }) => (
+							<TabIcon
+								icon={icons.home}
+								color={color}
+								name='Home'
+								focused={focused}
+							/>
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name='mapView'
+					options={{
+						title: "Map",
+						headerShown: false,
+						tabBarIcon: ({ color, focused }) => (
+							<TabIcon
+								icon={icons.map}
+								color={color}
+								name='Map'
+								focused={focused}
+							/>
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name='profile'
+					options={{
+						title: "Profile",
+						headerShown: false,
+						tabBarIcon: ({ color, focused }) => (
+							<TabIcon
+								icon={icons.profile}
+								color={color}
+								name='Profile'
+								focused={focused}
+							/>
+						),
+					}}
+				/>
+			</Tabs>
+		</>
 	);
 };
 
