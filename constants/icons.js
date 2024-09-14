d00ea8fd5e9ef3fae3449eeb83527d7aeaafa18e
0xsambit <177391633+0xsambit@users.noreply.github.com>
2024-09-14 21:25:21 +0530

    New icons added

--- constants/icons.js
+++ constants/icons.js
@@ -11,6 +11,11 @@ import rightArrow from "../assets/icons/right-arrow.png";
 import search from "../assets/icons/search.png";
 import vectorBlack from "../assets/icons/vectorBlack.png";
 import vectorBlack2 from "../assets/icons/vectorBlack2.png";
+import map from "../assets/icons/map.png";
+import privacy from "../assets/icons/private.png";
+import sun from "../assets/icons/sun.png";
+import waves from "../assets/icons/waves.png";
+import hand from "../assets/icons/hand.png";
 export default {
 	bookmark,
 	eyeHide,
@@ -25,4 +30,9 @@ export default {
 	search,
 	vectorBlack,
 	vectorBlack2,
+	map,
+	privacy,
+	sun,
+	waves,
+	hand,
 };
