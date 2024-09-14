350af58760e3786edcf065bc9241ecf8d2613339
0xsambit <177391633+0xsambit@users.noreply.github.com>
2024-09-14 21:29:50 +0530

    Add MapView and Profile components

---
+++ app/(tabs)/mapView.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MapView = () => {
+	return (
+		<View>
+			<Text>mapView</Text>
+		</View>
+	);
+};
+
+export default MapView;
+
+const styles = StyleSheet.create({});
