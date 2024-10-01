eefae1978f6007c53f21d938b351be242af571e5
0xsambit <177391633+0xsambit@users.noreply.github.com>
2024-10-01 23:34:28 +0530

    Add new videos to the videos constant

--- constants/videos.js
+++ constants/videos.js
@@ -1,5 +1,8 @@
 import goa from "../assets/videos/goa.mp4";
-
+import digha from "../assets/videos/digha.mp4";
+import laksha from "../assets/videos/lakshadweep.mp4";
 export default videos = {
 	goa,
+	digha,
+	laksha,
 };
