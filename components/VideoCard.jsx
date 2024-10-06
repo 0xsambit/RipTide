ec3bfde094894d0dd196c9076dec77748a9f0c9a
0xsambit <177391633+0xsambit@users.noreply.github.com>
2024-10-07 00:19:18 +0530

    Refactor VideoCard component and add temperature and cloud information
    - Refactor VideoCard component to include a Button and TouchableOpacity components
    - Add temperature and cloud information to the VideoCard component
    - Fetch temperature and cloud data from an API and display it in the VideoCard component

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,7 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import { Video } from "expo-av";
+import axios from "axios";
+import CustomButton from "./CustomButton";
 const VideoCard = ({ text, subtitle, uri }) => {
+	const [data, setData] = useState(null);
+
+	const handleData = async () => {
+		try {
+			const response = await axios.get(
+				`https://run.mocky.io/v3/1668f4a7-e999-4ec4-8f86-bc3849be1d34`
+			);
+			setData(response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		handleData();
+	}, []);
 	return (
 		<View>
 			<Video
@@ -14,6 +32,38 @@ const VideoCard = ({ text, subtitle, uri }) => {
 			/>
 			<Text style={styles.headtext}>{text}</Text>
 			<Text style={styles.subtitle}>{subtitle}</Text>
+			{data && (
+				<View
+					style={{
+						flexDirection: "row",
+						justifyContent: "space-between",
+						alignItems: "center",
+					}}>
+					<View>
+						<Text
+							style={{
+								fontFamily: "Poppins-SemiBold",
+								fontSize: 14,
+								marginTop: 10,
+							}}>
+							Temperature: {data.temperature}
+						</Text>
+						<Text
+							style={{
+								fontFamily: "Poppins-SemiBold",
+								fontSize: 14,
+								marginBottom: 10,
+							}}>
+							Cloud: {data.cloudCover}
+						</Text>
+					</View>
+					<TouchableOpacity
+						activeOpacity={0.7}
+						onPress={() => handleData(text)}>
+						<Text style={styles.button}>Check Now !!</Text>
+					</TouchableOpacity>
+				</View>
+			)}
 		</View>
 	);
 };
@@ -24,14 +74,20 @@ const styles = StyleSheet.create({
 	headtext: {
 		fontFamily: "Amita-Bold",
 		fontSize: 25,
-		marginTop: 10,
 		textAlign: "center",
 	},
 	subtitle: {
 		fontFamily: "Amita-Bold",
 		fontSize: 13,
 		width: "100%",
-		margin: "auto",
+		textAlign: "center",
+	},
+	button: {
+		fontFamily: "Poppins-Medium",
+		color: "white",
+		backgroundColor: "#1f292a",
+		padding: 8,
+		borderRadius: 10,
 		textAlign: "center",
 	},
 });
